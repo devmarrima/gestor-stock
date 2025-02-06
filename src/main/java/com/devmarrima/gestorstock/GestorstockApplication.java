6c62c611b8aaa5f82d4fbe91731e5f3dcddf6b3e
curso_básico_Dio <marrimacelso@gmail.com>
2025-02-05 22:56:27 -0300

    first commit

---
+++ src/main/java/com/devmarrima/gestorstock/GestorstockApplication.java
@@ -0,0 +1,13 @@
+package com.devmarrima.gestorstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmarrima/gestorstock/GestorstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmarrima.gestorstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
